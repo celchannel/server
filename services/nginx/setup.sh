86835fe39c507eec18aa1251711a2b8fe50c32e6
tlassere <tlassere@student.42mulhouse.fr>
2025-03-19 18:09:17 -0600

    Fix: setup nginx

--- services/nginx/setup.sh
+++ services/nginx/setup.sh
@@ -3,7 +3,7 @@
 nginx_cert_path="/etc/nginx/certs/nginx"
 
 openssl genrsa -out $nginx_cert_path.key 2048
-openssl req -new -key $nginx_cert_path.key -out $nginx_cert_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=NOUSLOL/OU=nousLOLSection/CN=ft_transcendence"
+openssl req -new -key $nginx_cert_path.key -out $nginx_cert_path.csr -nodes -subj "/C=celchannel/ST=France/L=chezmoi/O=NOUSLOL/OU=nousLOLSection/CN=celchannel"
 openssl x509 -req -days 3650 -in $nginx_cert_path.csr -signkey $nginx_cert_path.key -out $nginx_cert_path.crt
 
 while ! nc -z node 6900; do
