5ef77672440064f387b9b274b9048a18e2bc0e01
tlassere <tlassere@student.42mulhouse.fr>
2025-03-21 16:46:31 -0600

    Feat: container with volume

--- services/node/setup.sh
+++ services/node/setup.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+cat <<- EOF > .env
+	DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@postgresql:5432/$POSTGRES_DB?schema=public"
+	EOF
+
+until cd /app; do
+	sleep 0.2
+done
+
+npm install
 
 while ! nc -z postgresql 5432; do
     sleep 0.1
