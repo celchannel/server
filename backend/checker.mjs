741547062c467b688b2b852ee0c52125efde05f6
tlassere <tlassere@student.42mulhouse.fr>
2025-03-24 11:41:41 -0600

    Fix: checker check type

--- backend/checker.mjs
+++ backend/checker.mjs
@@ -12,6 +12,8 @@ export class Checker {
 	 */
 	static username(str)
 	{
+		if (typeof str != "string")
+			throw "The username must be a sting";
 		if (!(str && str.length >= 3 && str.length <= 40 && isAlnum(str)))
 			throw "The username must be between 3 and 20 characters long and must be composed exclusively of letters and numbers, with the exception of the following characters: _-";
 	}
@@ -22,7 +24,8 @@ export class Checker {
 	 */
 	static password(str)
 	{
-
+		if (typeof str != "string")
+			throw "The username must be a sting";
 		if (!(str && str.length >= 5 && str.length <= 250))
 			throw "The username must be between 5 and 250 characters long";
 	}
