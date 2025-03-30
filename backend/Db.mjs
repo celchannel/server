0ad7f2b1bee1a3c5e0b3d79c0675c570155105f2
tlassere <tlassere@student.42mulhouse.fr>
2025-03-29 19:38:50 -0600

    Feat: db SEISE throw

--- backend/Db.mjs
+++ backend/Db.mjs
@@ -1,39 +1,56 @@
 import { PrismaClient } from "@prisma/client";
+import { SEISE } from "./Error.mjs";
 
 const db = new PrismaClient();
 export class DbGet
 {
 	static async user(obj)
 	{
-		let result = null;
-		if (obj.username)
-			result = await db.user.findUnique({
-				where: {
-					name: obj.username
-				}
-			});
-		else if (obj.token)
-			result = await db.user.findUnique({
-				where: {
-					token: obj.token
-				}
-			});
-		return (result);
+		try
+		{
+			let result = null;
+			if (obj.username)
+				result = await db.user.findUnique({
+					where: {
+						name: obj.username
+					}
+				});
+			else if (obj.token)
+				result = await db.user.findUnique({
+					where: {
+						token: obj.token
+					}
+				});
+			return (result);
+		}
+		catch (err)
+		{
+			console.error(err);
+			throw new SEISE();
+		}
 	}
 
 	static async death(AreaSID, LevelName, Side, howMany)
 	{
-		return (await db.death.findMany({
-			orderBy: {
-				id: "desc"
-			},
-			where: {
-				AreaSID: AreaSID,
-				LevelName: LevelName,
-				Side: Side
-			},
-			take: howMany
-		}));
+		try
+		{
+			return (await db.death.findMany({
+				orderBy: {
+					id: "desc"
+				},
+				where: {
+					AreaSID: AreaSID,
+					LevelName: LevelName,
+					Side: Side
+				},
+				take: howMany
+			}));
+		}
+		catch (err)
+		{
+			console.error(err);
+			throw new SEISE();
+		}
 	}
 }
 
@@ -47,31 +64,47 @@ export class DbPush
 	 */
 	static async User(username, password, token)
 	{
-		const user = await db.user.create({
-			data:
-			{
-				name: username,
-				password: password,
-				token: token
-			}
-		});
-		console.log(user);
+		try
+		{
+			const user = await db.user.create({
+				data:
+				{
+					name: username,
+					password: password,
+					token: token
+				}
+			});
+			console.log(user);
+		}
+		catch (err)
+		{
+			console.error(err);
+			throw new SEISE();
+		}
 	}
 
 	static async Death(AreaSID, LevelName, Side, GoldenBerry, PositionX, PositionY, AuthorId)
 	{
-		const death = await db.death.create({
-			data:
-			{
-				AreaSID: AreaSID,
-				LevelName: LevelName,
-				Side: Side,
-				GoldenBerry: GoldenBerry,
-				PositionX: PositionX,
-				PositionY: PositionY,
-				AuthorId: AuthorId
-			}
-		});
-		console.log(death);
+		try
+		{
+			const death = await db.death.create({
+				data:
+				{
+					AreaSID: AreaSID,
+					LevelName: LevelName,
+					Side: Side,
+					GoldenBerry: GoldenBerry,
+					PositionX: PositionX,
+					PositionY: PositionY,
+					AuthorId: AuthorId
+				}
+			});
+			console.log(death);
+		}
+		catch (err)
+		{
+			console.error(err);
+			throw new SEISE();
+		}
 	}
 }
